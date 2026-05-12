9d45e50b86e21ad96bc03f18249025d5720c450c
Shubham Jain <shubham.ez556@gmail.com>
2026-05-12 12:58:27 +0530

    fix: vercel config + clean up next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  // If you later need to load images from external domains, add them here:
+  // images: {
+  //   remotePatterns: [
+  //     { protocol: 'https', hostname: 'example.com' },
+  //   ],
+  // },
 };
 
 module.exports = nextConfig;

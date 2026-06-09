afeb37357b3b2020dda5c30878177d6ab54b3ab8
Shubham Jain <shubham.ez556@gmail.com>
2026-06-09 13:02:16 +0530

    chore(config): update env.example, next.config, package.json

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,14 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: '*.r2.dev',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
     ],
   },
 };

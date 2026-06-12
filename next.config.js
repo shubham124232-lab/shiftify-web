b5e366228ef3749b6d2be486328bdcd341285d8e
shubham124232-lab <shubham124232@gmail.com>
2026-06-12 10:58:26 +0530

    fix the backend url

--- next.config.js
+++ next.config.js
@@ -9,14 +9,6 @@ const nextConfig = {
       { protocol: 'https', hostname: '*.r2.dev' },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/backend/:path*',
-        destination: 'https://midnightblue-gazelle-752413.hostingersite.com/:path*',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
